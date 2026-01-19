71ce8cc991bca9cbae0106a2b90598b348085dc7
Ayantik Sarkar <99005057+ayantik2006@users.noreply.github.com>
2026-01-19 11:10:07 +0530

    fix:chat timings

--- Talkative-NextJS/app/api/create-chat/route.ts
+++ Talkative-NextJS/app/api/create-chat/route.ts
@@ -3,38 +3,32 @@ import Chat from "@/models/Chat";
 import User from "@/models/User";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req:NextRequest){
-    const {chatId, content}=await req.json();
+export async function POST(req: NextRequest) {
+  const { chatId, content } = await req.json();
 
-    const now=new Date();
-    const date=`${now.getDate()}/${now.getMonth()+1}/${now.getFullYear()}`;
-    let time;
-    if(now.getHours()===0){
-        time="12";
-    }
-    else if(now.getHours()>=13 && now.getHours()<=23){
-        time=String(now.getHours()-12);
-    }
-    else{
-        time=String(now.getHours());
-    }
-    time+=":";
-    time+=String(now.getMinutes());
-    if(now.getHours()>=0 && now.getHours()<=12){
-        time+="am";
-    }
-    else time+="pm";
+  const now = new Date();
+  const date = `${now.getDate()}/${now.getMonth() + 1}/${now.getFullYear()}`;
 
-    const email=await getUserEmail();
-    const userData=await User.findOne({email:email});
-    const chatObjectData={
-        content:content,
-        time:`${time} ${date}`,
-        authorName:userData?.name,
-        authorId:userData?._id
-    }
-    await Chat.updateOne({_id:chatId},{$push:{chats:chatObjectData}})
-    const chatData=await Chat.findOne({_id:chatId});
-    const chats=chatData?.chats;
-    return NextResponse.json({chats:chats});
+  let hours = now.getHours();
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+
+  const period = hours >= 12 ? "pm" : "am";
+
+  hours = hours % 12;
+  hours = hours === 0 ? 12 : hours;
+
+  const time = `${hours}:${minutes}${period}`;
+
+  const email = await getUserEmail();
+  const userData = await User.findOne({ email: email });
+  const chatObjectData = {
+    content: content,
+    time: `${time} ${date}`,
+    authorName: userData?.name,
+    authorId: userData?._id,
+  };
+  await Chat.updateOne({ _id: chatId }, { $push: { chats: chatObjectData } });
+  const chatData = await Chat.findOne({ _id: chatId });
+  const chats = chatData?.chats;
+  return NextResponse.json({ chats: chats });
 }
